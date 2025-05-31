24a4a523e959f6c70b63abb56709158c69404550
LXQHC <1634166194@qq.com>
2025-05-31 15:26:40 +0800

    1
    1

---
+++ docs/card/banner.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+
+  //广告
+  var banSwiper = new Swiper('.ban', {
+      loop: true,
+      autoplay: true,
+      pagination: {
+          el: '.ban .swiper-pagination',
+          clickable: true,
+      },
+  });
+
+
+});
